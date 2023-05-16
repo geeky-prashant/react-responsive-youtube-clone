8fcb59f789153317108dce74bacacf45633af9b5
Prashant Bhardwaj <prashant.b@zengatv.com>
2023-05-16 09:36:35 +0530

    Hamburger Menu Fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
   const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
 
   return (
-    <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 p-3 md:px-5 bg-white dark:bg-black">
+    <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 p-3 md:px-5 bg-black">
       {loading && <Loader />}
 
       {/* Logo with Hamburger Menu  */}
